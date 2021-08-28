ed2838eadd06955effe7c7b1db42666bf078f339
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-08-28 11:30:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -100,7 +100,7 @@ async def start(bot, cmd):
                 [
                     [
                         InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
-                        InlineKeyboardButton("Other Bots", url="https://t.me/mazhatthullikal")
+                        InlineKeyboardButton("Other Bots", url="https://t.me/MOVIESLOKAM2")
                     ],
                     [
                         InlineKeyboardButton("About", callback_data="about")
